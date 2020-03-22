3e888ea0832df91e69b6fd18dd4fbc5c13891854
KielMC02 <49813048+KielMC02@users.noreply.github.com>
2020-03-22 16:11:07 -0400

    -Se perfecciono modelo estudiante.

--- G_D_Residencia_V1/G_D_Residencia_V1/Models/Model1.Designer.cs
+++ G_D_Residencia_V1/G_D_Residencia_V1/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\ewin mendez\Source\Repos\KielMC02\Residence_V1\G_D_Residencia_V1\G_D_Residencia_V1\Models\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Ewin Ezequiel Mendez\Source\Repos\KielMC02\Residence_V1\G_D_Residencia_V1\G_D_Residencia_V1\Models\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
